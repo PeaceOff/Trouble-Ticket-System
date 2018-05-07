abbf3d47f235db8da2a2cca224b1083c6472396e
Tiago Filipe <tiagoperolafilipe@gmail.com>
2018-05-07 11:38:19 +0100

    Added Login and Register views and viewmodels.

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApplication.Models;
 
 namespace WebApplication
 {
