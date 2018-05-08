9a939c2336a7e831b47c297d4c69e5fc734a4fea
Tiago Filipe <tiagoperolafilipe@gmail.com>
2018-05-08 20:14:46 +0100

    Added Ticket models in WebApp.

---
+++ WebApplication/Models/SecondTicket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class SecondTicket
+    {
+        public int ID { get; set; }
+
+        public String Title { get; set; }
+
+        public String Description { get; set; }
+
+        public String Answer { get; set; }
+
+        public String Authour { get; set; }
+
+        public String Solver { get; set; }
+
+        public String Department { get; set; }
+
+        public DateTime createdAt { get; set; }
+    }
+}
---
+++ WebApplication/Models/TroubleTicket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class TroubleTicket
+    {
+        public int ID { get; set; }
+
+        public String Authour { get; set; }
+
+        public String Solver { get; set; }
+
+        public String Title { get; set; }
+
+        public String Description { get; set; }
+
+        public String Answer { get; set; }
+
+        //TODO É Enum ou String?
+        public String State { get; set; }
+
+        public DateTime createdAt { get; set; }
+    }
+}
