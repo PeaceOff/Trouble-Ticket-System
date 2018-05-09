9dfe0e802c9ae78e0e702c69177d72120f45313a
David Azevedo <davidazevedo1996@hotmail.com>
2018-05-09 11:00:15 +0100

    Created message queue usage example

---
+++ RestAPI/Services/MessageQueue.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RestAPI.Services
+{
+    public class MessageQueue
+    {
+        private MessageQueue() {
+            int i = 1;
+            do
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = i + ": Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+                Console.WriteLine(" Press 'q' to exit.");
+                i++;
+            } while (Console.ReadLine() != "q");
+        }
+    }
+}
